e517283db1e7b627ae6f9041fc5ecc6a3ab89604
alexdad <alexdad@gmail.com>
2014-12-14 11:59:26 -0800

    Implement caching envelope object

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To avoid repeated matrx inversion, use the envelope object, caching the inverse
+## First create this object for a given matrix; then use casheSolve function to get it inverted 
+## This also works for more general solving of linear systems, since ... allows to pass parameter 'b' to the solve
+##
+## Usage (assuming square matrix ms)
+##	Creating envelope mms for the matrix ms
+##		mms<-makeCacheMatrix(ms)
+##	Getting inverse of ms. Note tracing "calculated"
+##		mis<-cacheSolve(mms) 
+##	Getting inverse of ms. Note tracing "getting cached"
+##		mis<-cacheSolve(mms) 
+##	Testing that the result is a diagonal ones matrix
+##	ms%*%mis
 
-## Write a short comment describing this function
-
+## Defines caching object for a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<-NULL
+    set<-function(y) {
+        x<<-y
+        inv<<-NULL
+    }
+    get<-function() x
+    setinv<-function(inverse) inv<<-inverse
+## Note - it is critically important to use inv<<-inverse and not inv<-inverse, 
+##        since the caller (cacheSolve) will need it to x$setinv(inv)
+    getinv<-function() inv
+    list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Returns an inverted matrix for 'x', using cache if it already did it in the past
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv<-x$getinv()
+    if (!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    message("calculating inverse")
+    mt<-x$get()
+    inv<-solve(mt,...)
+    x$setinv(inv)
+    inv
 }
